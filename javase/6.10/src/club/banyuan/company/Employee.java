51dc871b114721deed1825a437c8272b26091e7b
smy <s_life_l@163.com>
2020-06-12 16:48:43 +0800

    使用构造方法优化代码

--- javase/6.10/src/club/banyuan/company/Employee.java
+++ javase/6.10/src/club/banyuan/company/Employee.java
@@ -4,17 +4,36 @@ public class Employee{
 	private int workHour;
 	private String name;
 	private int baseSalary;
+	private static final int defaultBaseSalary = 3000;
 
-	public void setWorkHour(int hour){
+	public Employee(int workHour, String name, int baseSalary){
+		this.workHour = workHour;
+		this.baseSalary = baseSalary;
+		this.name = name;
+	}
+
+	public Employee(int workHour, String name){
+		this(workHour,name,defaultBaseSalary);
+	}
+
+	public Employee(){
+
+	}
+
+
+	public Employee setWorkHour(int hour){
 		workHour = hour;
+		return this;
 	}
 
-	public void setName(String aName){
+	public Employee setName(String aName){
 		name = aName;
+		return this;
 	}
 
-	public void setBaseSalary(int salary){
+	public Employee setBaseSalary(int salary){
 		baseSalary = salary;
+		return this;
 	}
 
 	public String getName(){
