a46b982976ffd6d6cbef47ddf3e316b38f09ddc1
smy <s_life_l@163.com>
2020-06-22 08:50:56 +0800

    class practice

---
+++ javase/6.20/src/club/banyuan/Book/PaperBook.java
@@ -0,0 +1,20 @@
+package club.banyuan.Book;
+
+public class PaperBook extends Book {
+
+    private double weightG;
+
+    public PaperBook(String title, double price, double cost, int pages, double weightG) {
+        super(title, price, cost, pages);
+        this.weightG = weightG;
+    }
+
+    public double getWeightG() {
+        return weightG;
+    }
+
+    public void setWeightG(double weightG) {
+        this.weightG = weightG;
+    }
+
+}
